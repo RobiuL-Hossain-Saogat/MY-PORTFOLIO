06cd420517eda7659e16b5740b20170804370906
RobiuL-Hossain-Saogat <gboy43610@gmail.com>
2025-12-14 12:05:13 +0600

    25th commit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ hamb.addEventListener('click',()=>{
 document.addEventListener('scroll', ()=>{
     var scroll_position=window.scrollY;
     if(scroll_position > 250){
-        header.style.backgroundColor='#29323c';
+        header.style.backgroundColor='#092543ff';
     }else{
         header.style.backgroundColor='transparent';
     }
